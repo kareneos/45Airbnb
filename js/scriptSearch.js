963d52898b8d54b8104b02d81f93103ca56a456e
Karen os <karenestelaos@gmail.com>
2016-12-16 18:14:06 -0600

    map api added

--- js/scriptSearch.js
+++ js/scriptSearch.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
     function(start, end, label) {
         
     });
+    //map
+    var mapOptions = {
+        center: new google.maps.LatLng(37.7831,-122.4039),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    new google.maps.Map(document.getElementById('map'), mapOptions);
 })
